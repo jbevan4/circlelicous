02f5db0991205a85a54aba48956fc670ef8c0e72
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-11-19 13:38:17 +0000

    Format public request code

--- 5_public_request.py
+++ 5_public_request.py
@@ -1,5 +1,6 @@
 from math import tan, radians
 
+
 def angle_to_grade(angle):
-  """Convert angle in degree to a percentage grade"""
-  return tan(radians(angle)) * 100 
+    """Convert angle in degree to a percentage grade"""
+    return tan(radians(angle)) * 100
