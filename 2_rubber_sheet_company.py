1fc4668f2700c793bdb99b9345a2098c246e7e07
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-11-19 13:32:55 +0000

    Fix ruber sheet company code

--- 2_rubber_sheet_company.py
+++ 2_rubber_sheet_company.py
@@ -1,9 +1,11 @@
+from circles.circle import Circle
+
 cuts = [1, 1.5, 0.8, 0.7]
-circles = Circle(cut) for cut in cuts
+circles = (Circle(cut) for cut in cuts)
 
 for circle in circles:
-  print(f"A circle with radius {circle.radius}\n" 
-        f"has a perimeter of {circle.permieter()}\n"
-        f"and a cold area of {circle.area()}\n")
-  circle.radius *= 1.1
-  print(f"and a warm area of {circle.area()}")
+    print(f"A circle with radius {circle.radius:.2f}\n"
+          f"has a perimeter of {circle.perimeter():.2f}\n"
+          f"and a cold area of {circle.area():.2f}")
+    circle.radius *= 1.1
+    print(f"and a warm area of {circle.area():.2f}")
